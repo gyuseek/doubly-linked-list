a1eb1251f2ae309c17ffe6726b3aaec773574b9b
gyuseek <51752966+gyuseek@users.noreply.github.com>
2019-06-18 23:47:41 +0900

    0618

--- DoublyLinkedList.c
+++ DoublyLinkedList.c
@@ -101,24 +101,66 @@ int RemoveNodeFromList(List* list, int i)
 //  리스트 제거하기 제거할게 없으면 -1 반환 성공시 0
 int DestroyList(List* list)
 {
+	int i;
+	int size = GetListSize(List * list);
+
+	for (i = 1; i < size + 1; i++)
+	{
+		RemoveNodeFromList(List * list, int i);
+	}
 	return 0;
 }
-
+/*
 int AddNodePtr(List* list, Node* node)
 {
-	return 0;
+	node = list->Head;
+	// list size 만큼 진행
+	while (node != NULL)
+	{
+		num++;
+		node = node->Next;
+	}
+	return node;
 }
-
-
+*/
 // i번째 노드의 포인터를 가져온다 
-
 Node* GetNode(List* list, int i)
 {
-	return NULL;
+	Node* horse = list->Head;
+	int cnt = 1;
+	//i번째 도달할 때까지 node 이동
+	while (cnt != i)
+	{
+		horse = horse->Next;
+		cnt++;
+	}
+	return horse;
 }
 
 //i 번째에 리스트를 추가한다 
-int AddList(List* list, int i)
+int AddList(List* list, List* inputlist, int i)
 {
+	size = GetListSize(List * list);
+	Node* anchor = GetNode(List * list, int i);
+
+	//list의 head 앞으로 들어갈 경우
+	if (i == 1)
+	{
+		list->Head->Prev = inputlist->Tail;
+		inputlist->Tail->Next = list->Head;
+	}
+	//list의 Tail 뒤로 들어가는 경우
+	else if (i == size + 1)
+	{
+		list->Tail->Next = inputlist->Head;
+		inputlist->Head->Prev = list->Tail;
+	}
+	else if ((i < size + 1) && (i > 1))
+	{
+		anchor->Next->Prev = inputlist->Tail;
+		inputlist->Tail->Next = anchor->Next;
+		anchor->Next = inputlist->Head;
+		inputlist->Head->Prev = anchor;
+	}
 	return 0;
 }
